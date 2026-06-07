30b6efc26eab13df072c495008df176f056b0fe5
Kata113 <katakung70@gmail.com>
2026-06-07 20:51:49 +0700

    Delete Zy directory

--- Zy/quiz.js
+++
@@ -1,9 +0,0 @@
-// แก้ไขฟังก์ชันโหลดให้มาอ่าน JSON ที่เราเตรียมมาจาก C++
-async function loadQuiz() {
-    const response = await fetch('quiz_data.json');
-    const wordPool = await response.json();
-    
-    // ตรงนี้คือจุดสำคัญ: เราใช้ลำดับตาม JSON เป๊ะๆ โดยไม่มีการ shuffle
-    qState.pool = wordPool; 
-    startQuiz();
-}

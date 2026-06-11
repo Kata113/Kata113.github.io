e1f6c706bc5e350ddee11f96421fe51740eaf4d9
Kata113 <katakung70@gmail.com>
2026-06-12 00:03:35 +0700

    Fix formatting and remove unnecessary comments in core.js

--- Beta/core.js
+++ Beta/core.js
@@ -1,4 +1,5 @@
-// --- GLOBAL STATES ---\nlet dict = [], dictSet = new Set(), wordsByL = {};
+// --- GLOBAL STATES ---
+let dict = [], dictSet = new Set(), wordsByL = {};
 let saved = JSON.parse(localStorage.getItem('zyz_sv') || '[]');
 let sFilters = [], qFilters = [], fId = 0;
 let currentResultsList = [], currentWordIndex = -1;
@@ -6,7 +7,7 @@ let activeSearchMode = 'subanagram';
 
 const letterScores = { A:1,E:1,I:1,O:1,U:1,L:1,N:1,S:1,T:1,R:1, D:2,G:2, B:3,C:3,M:3,P:3, F:4,H:4,V:4,W:4,Y:4, K:5, J:8,X:8, Q:10,Z:10 };
 const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const VOWELS = \"AEIOU\";
+const VOWELS = "AEIOU";
 
 const letterFrequencies = {
   A:9,B:2,C:2,D:4,E:12,F:2,G:3,H:2,I:9,J:1,K:1,L:4,
